b2127af5b144062449171694f987d1280930e6ae
John Fredrik Bendvold <johnfb@stud.ntnu.no>
2025-02-08 14:39:22 +0100

    new fitness

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,12 @@ fn main() {
     
     let best_solution = evolutionary_algorithm(
         &instance,
-        1000,
-        1000,
+        100,
+        5000,
         5,
-        0.5,
+        0.2,
         1.2,
-        10
+        100
     );
 
     plot_map(&best_solution, &instance.patients, &instance.depot);
@@ -295,7 +295,8 @@ fn edge_crossover(parent1: &Vec<Vec<usize>>, parent2: &Vec<Vec<usize>>) -> Vec<V
 fn fitness(solution: &Vec<Vec<usize>>, instance: &Instance) -> f64 {
     let mut total_travel_time = 0.0;
     let mut total_penalty = 0.0;
-    let penalty_factor = 100.0; // Higher value means higher penalty
+    let penalty_factor = 2.0; // Higher value means higher penalty
+    let penalty_factor_time = 6.0; // Higher value means higher penalty
 
     // Calculate the total travel time for each nurse
     let mut nurses = instance.nurses.clone();
@@ -306,6 +307,7 @@ fn fitness(solution: &Vec<Vec<usize>>, instance: &Instance) -> f64 {
 
         // Calculate the travel time and capacity for each patient in the route
         for patient_id in route {
+            let mut wait_time = 0.0;
             let patient = &instance.patients[&patient_id.to_string()];
             // Print the nurse and patient ID
             //println!("Last patient: {:?}, Current patient: {:?}", last_patient, patient_id);
@@ -313,17 +315,20 @@ fn fitness(solution: &Vec<Vec<usize>>, instance: &Instance) -> f64 {
             // Calculate the travel time from the last patient to the current patient
             let travel_time = instance.travel_times[last_patient][*patient_id];
 
+            // Add the travel time as a penalty, since then nurses will be penalized for traveling too much between patients
+            total_penalty += travel_time * penalty_factor_time;
+
             // Check if the nurse visits the patient too early
-            if patient.start_time > nurse.get_current_total_time() + travel_time {
-                total_penalty += penalty_factor * (patient.start_time - nurse.get_current_total_time() - travel_time);
+            if patient.start_time > (nurse.get_current_travel_time() + travel_time) {
+                wait_time = patient.start_time - (nurse.get_current_travel_time() + travel_time);
             }
 
             // Add the travel time to the nurse's current travel time
-            nurse.set_current_travel_time(nurse.get_current_travel_time() + travel_time + patient.care_time);
+            nurse.set_current_travel_time(nurse.get_current_travel_time() + travel_time + patient.care_time + wait_time);
 
             // Check if the nurse visits the patient too late
-            if patient.end_time < nurse.get_current_total_time() {
-                total_penalty += penalty_factor * (nurse.get_current_total_time() - patient.end_time);
+            if patient.end_time < nurse.get_current_travel_time() {
+                total_penalty += penalty_factor * (nurse.get_current_travel_time() - patient.end_time);
             }
 
             // Add the patient's demand to the nurse's current load
@@ -340,6 +345,11 @@ fn fitness(solution: &Vec<Vec<usize>>, instance: &Instance) -> f64 {
         // Check if the nurses capacity is exceeded
         if nurse.get_current_load() as f64 > nurse.get_capacity() as f64 {
             total_penalty += penalty_factor * (nurse.get_current_load() as f64 - nurse.get_capacity() as f64);
+        }
+
+        // Check if the nurse returns to the depot too late
+        if nurse.get_current_travel_time() > instance.depot.return_time {
+            total_penalty += penalty_factor * (nurse.get_current_travel_time() - instance.depot.return_time);
         }
 
         // Add the nurse's travel time to the total travel time
